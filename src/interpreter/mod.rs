f10d1ec96af5b9119f016e7f95b06ff070d618b2
EdmundGoodman <egoodman3141@gmail.com>
2023-08-22 19:00:11 +0100

    Get ready for starting fetch stage

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -1,26 +1,35 @@
 use std::time::Duration;
 
 pub const MEM_SIZE: usize = 4096;
+pub const STACK_SIZE: usize = 16;
 pub const NUM_GP_REG: usize = 16;
 
 pub struct VirtualMachine {
-    pub mem: [u8; MEM_SIZE],   // Addressable memory
-    pub vxs: [u8; NUM_GP_REG], // Vx registers
-    pub i: u16,                // I register
-    pub dt: u8,                // Display timer
-    pub st: u8,                // Sound timer
-    pub display: chip8_base::Display,
+    pub mem: [u8; MEM_SIZE],          // Addressable memory
+    pub stack: [u16; STACK_SIZE],     // Stack memory
+    pub vxs: [u8; NUM_GP_REG],        // Vx registers (VF as flag)
+    pub i: u16,                       // I register
+    pub pc: u16,                      // Program counter
+    pub sp: u8,                       // Stack pointer
+    pub dt: u8,                       // Display timer
+    pub st: u8,                       // Sound timer
+    pub display: chip8_base::Display, // Display output
+    speed: Duration,                  // Clock period
 }
 
 impl VirtualMachine {
-    pub fn new() -> VirtualMachine {
+    pub fn new(speed: Duration) -> VirtualMachine {
         VirtualMachine {
             mem: [0; MEM_SIZE],
+            stack: [0; STACK_SIZE],
             vxs: [0; NUM_GP_REG],
             i: 0,
+            pc: 0,
+            sp: 0,
             dt: 0,
             st: 0,
             display: [[chip8_base::Pixel::Black; 64]; 32],
+            speed,
         }
     }
 }
@@ -30,8 +39,8 @@ impl chip8_base::Interpreter for VirtualMachine {
         None
     }
 
-    fn speed(&self) -> std::time::Duration {
-        Duration::new(1, 0)
+    fn speed(&self) -> Duration {
+        self.speed
     }
 
     fn buzzer_active(&self) -> bool {
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 mod interpreter;
 
 use crate::interpreter::VirtualMachine;
+use std::time::Duration;
+
+fn period_from_freq(freq: u64) -> Duration {
+    Duration::from_micros(1000000 / freq)
+}
 
 fn main() {
-    chip8_base::run(VirtualMachine::new());
+    let speed: Duration = period_from_freq(700);
+    let vm: VirtualMachine = VirtualMachine::new(speed);
+    chip8_base::run(vm);
 }
